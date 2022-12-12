f72aa67ae44cbbdce7d3ddd28bd67a9fdc1874d5
gutierrezK30 <kgutierrez301190@gmail.com>
2022-12-12 10:26:23 -0500

    actualizando conexion

--- include/conexion.php
+++ include/conexion.php
@@ -1,9 +1,12 @@
 <?php
 // conexion utiliza servidor-usuario-password-base de datos
-$conexion = mysqli_connect("localhost", "root", "root", "SisAcad");
+//$conexion = mysqli_connect("localhost", "root", "root", "SisAcad");
+
+$conexion = mysqli_connect("localhost", "desarro1_katia", "RG6yHK8QuJsq", "desarro1_katiagutierrez");
+
 if ($conexion) {
 
 } else {
-   echo "conexion fallida";
+   echo "Error de conexion a la base de datos";
 }
 ?>
